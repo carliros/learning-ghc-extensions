641ac82d517f8908a3303b491b2c717ee1946853
Carlos Gomez <carliros.g@gmail.com>
2021-02-24 14:33:25 -0400

    Add View Patterns extension

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,24 @@
+{-# LANGUAGE ViewPatterns #-}
 module Main where
+
+{- ######### View Patterns extension -}
+-- example taken from https://jsdw.me/posts/haskell-language-extensions/#viewpatterns
+-- The general idea is that you can execute the case pattern match in any computation. 
+
+someList :: [(String, Int)]
+someList = [("n1", 1), ("n2", 2)]
+
+getOrDefault :: String -> Int
+getOrDefault name = case (lookup name someList) of
+  Just v -> v
+  Nothing -> 5
+
+getOrDefault' :: String -> Int
+getOrDefault' (\n -> lookup n someList -> Just v) = v
+getOrDefault' _ = 5
+
+-- I wonder how it would be with a bigger data type, will it execute the computation each time?
+data Contact = Person String | Institute String | Reference String
 
 main :: IO ()
 main = do
